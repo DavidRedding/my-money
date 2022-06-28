0343c59796e12225d18ad1a1e83cfe6dd6030be9
David Redding <redding.david35@gmail.com>
2022-06-27 21:00:25 -0500

    pages & routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-const App = () => <div className="">Hi</div>;
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// pages & components
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+
+const App = () => (
+  <div className=" min-h-screen">
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
