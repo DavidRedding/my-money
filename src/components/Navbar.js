6f7d07f0c78598392379386f484fc99fd5792984
David Redding <redding.david35@gmail.com>
2022-06-29 10:29:01 -0500

    Sign Up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
+
 // prettier-ignore
 const Navbar = () => (
-  <nav className="px-2 py-4 bg-green-50">
+  <nav className="p-4 bg-green-100">
     <ul className="flex items-center max-w-[960px] mx-auto ">
       <li className="mr-auto text-xl font-bold"><Link to="/">myMoney</Link></li>
       
-      <li><Link to="/login">Login</Link></li>
-      <li className="ml-4"><Link to="/signup">Signup</Link></li>
+      <li><Link to="/login">login</Link></li>
+      <li className="ml-4"><Link to="/signup">sign up</Link></li>
     </ul>
   </nav>
 );
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,3 +1,55 @@
-const Signup = () => <div className="">Signup</div>;
+import { useState } from 'react';
+// prettier-ignore
+
+const Signup = () => {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className=" mx-auto max-w-[360px] p-4 my-14">
+      <h2 className="text-xl font-bold">sign up</h2>
+
+      <label className="block my-5">
+        <span className="block mb-1 ">display name :</span>
+        <input
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          className="w-full px-2 py-1 border border-gray-500 "
+          type="text"
+        />
+      </label>
+
+      <label className="block my-5">
+        <span className="block my-1 ">email :</span>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="w-full px-2 py-1 border border-gray-500 "
+          type="email"
+        />
+      </label>
+
+      <label className="block my-5">
+        <span className="block my-1 ">password :</span>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className="w-full px-2 py-1 border border-gray-500 "
+          type="password"
+        />
+      </label>
+
+        <button className="block px-3 py-1 text-green-600 border border-green-600 rounded hover:bg-green-600 hover:text-green-50">
+          Sign Up
+        </button>
+    </form>
+  );
+};
 
 export default Signup;
