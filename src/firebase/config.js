1e8b3b90af5a1ed6597dfb576c4665317fd800a3
David Redding <redding.david35@gmail.com>
2022-06-30 17:16:06 -0400

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDrgvigzxrxeOfzErYKrgAIssxb1sACmBg',
+  authDomain: 'mymoney-46a04.firebaseapp.com',
+  projectId: 'mymoney-46a04',
+  storageBucket: 'mymoney-46a04.appspot.com',
+  messagingSenderId: '415357467773',
+  appId: '1:415357467773:web:c035a138eabc5b16988514',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
