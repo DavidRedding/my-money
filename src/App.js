d8bbbbb7fa75a9505b0e1bfc3edeb7108161628c
David Redding <redding.david35@gmail.com>
2022-07-18 10:09:08 -0500

    Auth State Changes Build + Implementation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
 import Home from './pages/home/Home';
@@ -6,23 +7,29 @@ import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 
-const App = () => (
-  <div className=" min-h-screen">
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />{' '}
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  </div>
-);
+const App = () => {
+  const { authIsReady } = useAuthContext();
+
+  return (
+    <div className="min-h-screen ">
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
+    </div>
+  );
+};
 
 export default App;
