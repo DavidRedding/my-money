4c5188fe2c1747f4cd1bd2970805369a15c2574b
David Redding <redding.david35@gmail.com>
2022-09-22 08:51:38 -0500

    new logo, navbar visibility/hidden errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
 import useLogout from '../hooks/useLogout';
+import rm2 from '../assets/rm2.svg';
 
 // prettier-ignore
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
   return (
-    <nav className="py-4 bg-green-50">
+    <nav className="py-4 bg-green-50 ">
       <ul className="flex items-center max-w-6xl px-10 mx-auto ">
-        <li className="mr-auto text-xl font-bold"><Link to="/">myMoney</Link></li>
+        <li className="mr-auto text-xl font-bold"><Link to="/"><img className="h-6" src={rm2} alt="Logo" /></Link></li>
 
         {!user && (
           <>
@@ -20,7 +21,9 @@ const Navbar = () => {
 
         {user && (
           <>
-           <li>{`hello, ${user.displayName}`}</li>
+           {/* Code doesn't work as intended :/
+           <li className='hidden xs:visible'>{`hello, ${user.displayName}`}</li> */}
+           <li className='text-green-50 xs:text-black'>{`hello, ${user.displayName}`}</li>
             <li> 
               <button
                 className="block px-3 py-1 ml-4 text-green-600 border border-green-600 rounded hover:bg-green-600 hover:text-green-50"
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,13 +8,14 @@ const Home = () => {
   const { documents, error } = useCollection('transactions', ['uid', '==', user.uid], ['createdAt', 'desc']);
 
   return (
-    <div className="grid max-w-6xl grid-cols-3 gap-16 p-10 mx-auto">
-      <div className="col-span-2">
+    <div className="grid max-w-6xl grid-cols-3 gap-16 p-10 mx-auto sm:bg-white">
+      <div className="order-1 col-span-3 sm:-order-1 sm:col-span-2">
+        <h3 className="text-lg font-bold text-green-600 sm:hidden">Recent Transactions</h3>
         {error && <h1>{error}</h1>}
         {documents && <TransactionList documents={documents} />}
       </div>
 
-      <div className="col-span-1">
+      <div className="col-span-3 sm:col-span-1">
         <TransactionForm uid={user.uid} />
       </div>
     </div>
